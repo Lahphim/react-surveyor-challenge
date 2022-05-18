4401086ceb554c4bab4f4261712bad6b160f9bca
Somsak Arnon <lahphim@gmail.com>
2022-05-18 21:00:26 +0700

    [#4] Enable tailing slash

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   pageExtensions: ['page.tsx'],
+  reactStrictMode: true,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
