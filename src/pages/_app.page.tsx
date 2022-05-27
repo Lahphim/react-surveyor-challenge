e55ef1fdd395027c05585af7aecd046ec4e43949
Somsak Arnon <lahphim@gmail.com>
2022-05-27 14:57:16 +0700

    [#6] Refactor app and home page

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,9 +1,23 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import { SEO_TITLE, SEO_DESCRIPTION } from '@/constants/seo';
+import { theme } from '@/theme/index';
+
+import '@/styles/globals.scss';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>{SEO_TITLE}</title>
+        <meta name="description" content={SEO_DESCRIPTION} />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
