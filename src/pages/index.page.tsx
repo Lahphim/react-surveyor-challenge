75e4e7ab4f9e8d953031b51c18f5c37a366c9d9d
Somsak Arnon <lahphim@gmail.com>
2022-05-27 15:35:34 +0700

    [#6] Preview all basic components

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,28 @@
+import { useRef } from 'react';
+
+import { SearchIcon } from '@chakra-ui/icons';
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  IconButton,
+  Input,
+  useDisclosure,
+} from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Image from 'next/image';
 
+import AvatarBot from '@/components/AvatarBot';
 import styles from '@/styles/Home.module.scss';
 
 const Home: NextPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef<HTMLButtonElement>(null);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -11,39 +30,122 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.page.tsx</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+          <div className={styles.card}>
+            <h2>:: Avatar</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              <AvatarBot email="dev@nimblehq.co"></AvatarBot>
             </p>
-          </a>
+          </div>
+
+          <div className={styles.card}>
+            <h2>:: Normal Button</h2>
+            <p>
+              <Button>Button</Button>
+            </p>
+          </div>
+
+          <div className={styles.card}>
+            <h2>:: Translucent Button</h2>
+            <p>
+              <Button colorScheme="translucent">Translucent Button</Button>
+            </p>
+          </div>
+
+          <div className={styles.card}>
+            <h2>:: Long Button</h2>
+            <p>
+              <Button>Looooong Content Button</Button>
+            </p>
+          </div>
+
+          <div className={styles.card}>
+            <h2>:: Small Button</h2>
+            <p>
+              <Button size="sm">Small Button</Button>
+            </p>
+          </div>
+
+          <div className={styles.card}>
+            <h2>:: Translucent Button</h2>
+            <p>
+              <Button colorScheme="translucent" size="sm">
+                Translucent Button
+              </Button>
+            </p>
+          </div>
+
+          <div className={styles.card}>
+            <h2>:: Solid Icon Button</h2>
+            <p>
+              <IconButton
+                variant="circle"
+                aria-label="Search database"
+                icon={<SearchIcon />}
+              />
+            </p>
+          </div>
+
+          <div className={styles.card}>
+            <h2>:: Ghost Icon Button</h2>
+            <p>
+              <IconButton
+                variant="circle-ghost"
+                aria-label="Search database"
+                icon={<SearchIcon color="white" />}
+              />
+            </p>
+          </div>
+
+          <div className={styles.card}>
+            <h2>:: Input Field</h2>
+            <p>
+              <Input placeholder="Placeholder" />
+            </p>
+          </div>
+
+          <div className={styles.card}>
+            <h2>:: Alert Dialog</h2>
+            <p>
+              <Button colorScheme="blue" onClick={onOpen}>
+                Open Alert
+              </Button>
+
+              <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+              >
+                <AlertDialogOverlay>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>Warning!</AlertDialogHeader>
+
+                    <AlertDialogBody>
+                      Are you sure you want to quit the survey?
+                    </AlertDialogBody>
+
+                    <AlertDialogFooter>
+                      <Button
+                        colorScheme="translucent"
+                        size="sm"
+                        onClick={onClose}
+                      >
+                        Yes
+                      </Button>
+                      <Button
+                        size="sm"
+                        ref={cancelRef}
+                        onClick={onClose}
+                        ml={3}
+                      >
+                        Cancel
+                      </Button>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialogOverlay>
+              </AlertDialog>
+            </p>
+          </div>
         </div>
       </main>
 
