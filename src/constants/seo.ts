b56f8ceed04b94f4228bd3cb8e939a8dc135ea5e
Somsak Arnon <lahphim@gmail.com>
2022-05-28 20:44:26 +0700

    [#6] Remove unused code

--- src/constants/seo.ts
+++
@@ -1,5 +0,0 @@
-const SEO_TITLE = 'Internal Certification : React + Next.js';
-const SEO_DESCRIPTION =
-  'A project to challenge ourselves with specific web stacks to achieve the Internal Certification. 🚀';
-
-export { SEO_TITLE, SEO_DESCRIPTION };
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
-import { SEO_TITLE, SEO_DESCRIPTION } from '@/constants/seo';
 import { theme } from '@/theme/index';
 
 import '@/styles/globals.scss';
@@ -10,14 +8,6 @@ import '@/styles/globals.scss';
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <title>{SEO_TITLE}</title>
-        <meta name="description" content={SEO_DESCRIPTION} />
-      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
