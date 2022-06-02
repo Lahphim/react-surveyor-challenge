197d3560b73ba5a5231d3330beb440ba37c246eb
Somsak Arnon <lahphim@gmail.com>
2022-06-02 19:30:01 +0700

    [#7] Make app support with layout

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,13 +1,34 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import type { AppProps } from 'next/app';
+import type { ReactElement, ReactNode } from 'react';
 
+import { ChakraProvider } from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import { SEO_TITLE, SEO_DESCRIPTION } from '@/constants/seo';
 import { theme } from '@/theme/index';
 
-import '@/styles/globals.scss';
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
-function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
+    <ChakraProvider resetCSS theme={theme}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>{SEO_TITLE}</title>
+        <meta name="description" content={SEO_DESCRIPTION} />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
