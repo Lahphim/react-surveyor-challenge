475d81715f786789e30328a01ece385093bfd9c0
Somsak Arnon <lahphim@gmail.com>
2022-06-08 04:36:32 +0700

    [#8] Update custom theme

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,6 +1,7 @@
 import Avatar from './avatar';
 import Button from './button';
+import FormLabel from './form-label';
 import Input from './input';
 import Modal from './modal';
 
-export { Avatar, Button, Input, Modal };
+export { Avatar, Button, FormLabel, Input, Modal };
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,7 @@
 import { extendTheme } from '@chakra-ui/react';
 import type { ThemeConfig } from '@chakra-ui/react';
 
-import { Avatar, Button, Input, Modal } from '@/theme/components';
+import { Avatar, Button, FormLabel, Input, Modal } from '@/theme/components';
 import foundations from '@/theme/foundations';
 
 import styles from './styles';
@@ -20,6 +20,7 @@ export const theme = extendTheme({
   components: {
     Avatar,
     Button,
+    FormLabel,
     Input,
     Modal,
   },
