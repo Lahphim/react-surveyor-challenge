572b0889764370b52320acc301482101b969f19e
Somsak Arnon <lahphim@gmail.com>
2022-06-16 11:10:10 +0700

    [#9] Fix typo

--- src/helpers/String/index.test.ts
+++ src/helpers/String/index.test.ts
@@ -5,7 +5,7 @@ import { capitalize } from './';
 describe('String helper', () => {
   describe('capitalize', () => {
     it('returns capitalize text', () => {
-      const text = `${faker.company}`.toLowerCase;
+      const text = `${faker.company}`.toLowerCase();
 
       expect(capitalize(`the ${text}`)).toBe(`The ${text}`);
     });
