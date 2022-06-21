1979cdbef82115132aeafa30a3b46e495d73ea88
Somsak Arnon <lahphim@gmail.com>
2022-06-21 23:10:23 +0700

    [#62] Add some tests for the sample reducer

--- src/reducers/Sample/index.ts
+++ src/reducers/Sample/index.ts
@@ -8,14 +8,16 @@ export const initialState: SampleState = {
   value: 0,
 };
 
+export const sampleReducers = {
+  increment: (state: SampleState) => {
+    state.value += 1;
+  },
+};
+
 export const sampleSlice = createSlice({
   name: 'sampleThing',
   initialState,
-  reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-  },
+  reducers: sampleReducers,
 });
 
 export const sampleActions = sampleSlice.actions;
