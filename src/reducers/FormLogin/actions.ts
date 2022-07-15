3d5c2b4f3bca11fc59665ef03c10d109bfd47519
Somsak Arnon <lahphim@gmail.com>
2022-07-15 11:52:44 +0700

    [#37] Remove eslint rules

--- src/reducers/FormLogin/actions.ts
+++ src/reducers/FormLogin/actions.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ActionReducerMapBuilder, createAsyncThunk } from '@reduxjs/toolkit';
 
 import authenticationAdapter from '@/adapters/Authentication';
