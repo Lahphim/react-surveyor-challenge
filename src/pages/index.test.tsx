94cb6eb386d92264c70465dae2dc7c04bf0baf41
Somsak Arnon <lahphim@gmail.com>
2022-07-24 16:01:00 +0700

    [#69] Add new home page

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -1,13 +1,24 @@
 import { render, screen } from '@testing-library/react';
 
-import Home, { homeDataTestIds } from './index.page';
+import { cornerLinkTestIds } from '@/components/CornerLink';
+import { welcomeTestIds } from '@/components/Welcome';
 
-describe('Home', () => {
-  it('renders a heading', () => {
+import Home from './index.page';
+
+describe('styleguide', () => {
+  it('renders the welcome component', () => {
     render(<Home />);
 
-    const heading = screen.getByTestId(homeDataTestIds.heading);
+    const welcome = screen.getByTestId(welcomeTestIds.base);
 
-    expect(heading).toBeVisible();
+    expect(welcome).toBeVisible();
+  });
+
+  it('renders a link to styleguide', () => {
+    render(<Home />);
+
+    const styleguideLink = screen.getByTestId(cornerLinkTestIds.base);
+
+    expect(styleguideLink).toBeVisible();
   });
 });
